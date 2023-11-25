1ef7bbd030b72134c496c98168c444ad9052b359
Sadik Ahmmed Tonmoy <Sadikahmed2258@gmail.com>
2023-11-25 14:29:40 +0600

    last

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -3,9 +3,9 @@ import { userController } from './user.controller';
 
 const router = express.Router();
 
-router.post('/api/users', userController.createUser);
-router.get('/api/users', userController.getAllUsers);
-router.get('/api/users/:userId', userController.getSingleUser);
-router.delete('/api/users/:userId', userController.deleteUser);
+router.post('/', userController.createUser);
+router.get('/', userController.getAllUsers);
+router.get('/:userId', userController.getSingleUser);
+router.delete('/:userId', userController.deleteUser);
 
 export const userRoutes = router;
